727d3d917784f7b73e223095feb134eaa641c76f
Douglas Pacheco <douglas.pacheco@version1.com>
2023-10-03 12:17:16 +0100

    k8s updates

--- terraform/k8s/main.tf
+++ terraform/k8s/main.tf
@@ -34,3 +34,30 @@ resource "aws_eks_cluster" "k8s_training" {
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [ aws_iam_policy.k8s_policy ]
 }
+
+resource "aws_eks_node_group" "k8s_training_node_group" {
+  cluster_name    = aws_eks_cluster.k8s_training.name
+  node_group_name = "nodegroup_${terraform.workspace}_k8s${var.module_suffix}"
+  node_role_arn   = aws_iam_role.k8s_role.arn
+  subnet_ids      = module.vpc.subnet_id_private[0]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+#  lifecycle {
+#    ignore_changes = [scaling_config[0].desired_size]
+#  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_policy.k8s_policy
+  ]
+}
