e92a4426d79ceaf499142218a094901b56ca93cd
Douglas Pacheco <douglas.pacheco@version1.com>
2023-10-04 13:18:46 +0100

    Using EKS module

--- terraform/k8s/outputs.tf
+++ terraform/k8s/outputs.tf
@@ -1,7 +1,7 @@
-output "endpoint" {
-  value = aws_eks_cluster.k8s_training.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.k8s_training.certificate_authority[0].data
-}
+#output "endpoint" {
+#  value = aws_eks_cluster.k8s_training.endpoint
+#}
+#
+#output "kubeconfig-certificate-authority-data" {
+#  value = aws_eks_cluster.k8s_training.certificate_authority[0].data
+#}
